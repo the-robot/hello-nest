fded10cbf3b88f026ffec981e4972e649555616f
ghost <9334746+the-robot@users.noreply.github.com>
2024-02-09 01:26:12 +0800

    feat: added scenarios for get

--- k6/get-feedbacks.js
+++ k6/get-feedbacks.js
@@ -1,21 +1,44 @@
-import http from 'k6/http';
-import { check, sleep } from 'k6';
+import http from 'k6/http'
+import { check, sleep } from 'k6'
 
 // Load environment variables from .env
-const api = __ENV.API || process.env.API || "http://localhost:3000";
+const api = __ENV.API || process.env.API || 'http://localhost:3000'
 
-export let options = {
-  vus: 10, // virtual users
-  duration: '30s',
-};
+// normal load
+export const options = {
+  stages: [
+    { duration: '5s', target: 20 },
+    { duration: '1m', target: 20 },
+    { duration: '5s', target: 0 },
+    { duration: '20s', target: 0 },
+  ],
+  thresholds: {
+    http_req_failed: ['rate<0.01'], // http errors should be less than 5%
+    http_req_duration: ['p(95)<60'], // 99% of requests should be below 40ms
+  },
+}
+
+// high load
+// export const options = {
+//   stages: [
+//     { duration: '5s', target: 100 },
+//     { duration: '1m', target: 100 },
+//     { duration: '5s', target: 0 },
+//     { duration: '20s', target: 0 },
+//   ],
+//   thresholds: {
+//     http_req_failed: ['rate<0.01'], // http errors should be less than 5%
+//     http_req_duration: ['p(95)<60'], // 99% of requests should be below 40ms
+//   },
+// }
 
 export default function () {
-  let res = http.get(`${api}/feedback?page=1`);
+  let res = http.get(`${api}/feedback?page=1`)
 
   // Check if request was successful
   check(res, {
     'status is 200': (r) => r.status === 200,
-  });
+  })
 
-  sleep(1);
+  sleep(1)
 }
