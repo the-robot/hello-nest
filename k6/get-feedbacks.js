93b1789f1f4307563b7bef2467b33eef102e1f85
ghost <9334746+the-robot@users.noreply.github.com>
2024-02-08 10:25:06 +0800

    feat: get feedbacks

---
+++ k6/get-feedbacks.js
@@ -0,0 +1,23 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env
+dotenv.config();
+const api = __ENV.API || process.env.API || "http://localhost:3000";
+
+export let options = {
+  vus: 10, // virtual users
+  duration: '30s',
+};
+
+export default function () {
+  let res = http.get(`${api}/feedback?page=1`);
+
+  // Check if request was successful
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+  });
+
+  sleep(1);
+}
